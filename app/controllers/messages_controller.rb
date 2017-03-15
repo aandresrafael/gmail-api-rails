6b67eb6c5f3c8d203327300114628dda3851c5ca
Andres Aguilar <andres@assembla.com>
2017-03-15 09:35:15 -0600

    Add validation when token expired.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,10 +4,12 @@ class MessagesController < ApplicationController
     return unless session[:google_access_token]
 
     api_client = SaneBox::GClient.get_client
-    @auth = api_client.authorization.dup
-    @auth.update_token!(session[:google_access_token])
+    auth = api_client.authorization.dup
+    auth.update_token!(session[:google_access_token])
 
-    api_client.authorization = @auth
+    redirect_to google_oauth_path and return if auth.expired?
+
+    api_client.authorization = auth
 
     user_id = session[:google_access_token]['user_id']
     gmail_client = SaneBox::GmailClient.new(api_client, user_id)
